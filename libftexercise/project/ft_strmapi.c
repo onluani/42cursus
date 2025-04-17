56792022e1a0e702833a69bb9c15ec101c802998
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-04-17 15:24:19 +0200

    Create ft_strmapi.c

---
+++ libftexercise/project/ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antsitsk <antsitsk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 14:36:30 by antsitsk          #+#    #+#             */
+/*   Updated: 2025/04/17 15:07:48 by antsitsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s || !f)
+		return (NULL);
+	i = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	str[i] = '\0';
+	j = 0;
+	while (j < i)
+	{
+		str[j] = f(j, s[j]);
+		j++;
+	}
+	return (str);
+}
