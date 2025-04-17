5300a646078320412723033e90d8405f553a28d4
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-04-17 16:10:43 +0200

    Add files via upload

---
+++ piscine/C03/projectc03/ex04/ft_strstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antsitsk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 14:24:27 by antsitsk          #+#    #+#             */
+/*   Updated: 2025/03/04 15:58:11 by antsitsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int		i;
+	char	*temp;
+
+	temp = str;
+	if (*to_find == '\0')
+		return (str);
+	while (*temp)
+	{
+		i = 0;
+		if (*temp == to_find[0])
+		{
+			while (temp[i] == to_find[i] && to_find[i])
+				i++;
+			if (to_find[i] == '\0')
+				return (temp);
+		}
+		temp++;
+	}
+	return (0);
+}
