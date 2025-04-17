5bace457ec271baf4c43c07837bb4d6b8b15d3c8
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-04-17 15:24:41 +0200

    Update libft.h

--- libftexercise/project/libft.h
+++ libftexercise/project/libft.h
@@ -6,7 +6,7 @@
 /*   By: antsitsk <antsitsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 14:35:35 by antsitsk          #+#    #+#             */
-/*   Updated: 2025/04/15 16:19:13 by antsitsk         ###   ########.fr       */
+/*   Updated: 2025/04/17 14:37:25 by antsitsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
