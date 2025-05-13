687bbd54bb96f386add71c4f08f466cce8c9a7ba
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-05-13 14:05:17 +0200

    Update ft_printf.h

--- projectprint/ft_printf.h
+++ projectprint/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: antsitsk <antsitsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 16:24:01 by antsitsk          #+#    #+#             */
-/*   Updated: 2025/04/30 16:26:32 by antsitsk         ###   ########.fr       */
+/*   Updated: 2025/05/13 14:05:08 by antsitsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
+
+int		ft_printf(const char *format, ...);
+void	parse_conversions(char *str, int *length, va_list arguments);
+int		parse_input(const char *str, va_list arguments, int length);
+int		ft_putchar(char c, int *length);
+void	ft_putnbr(int nb, int *length);
+void	ft_putuns(unsigned int nb, int *length);
+void	ft_puthex(unsigned long long nb, int *length, char *str);
+void	c_type(va_list arguments, int *length);
+void	s_type(va_list arguments, int *length);
+void	d_i_type(va_list arguments, int *length);
+void	u_type(va_list arguments, int *length);
+void	x_type(va_list arguments, int *length, char *str);
+void	p_type(va_list arguments, int *length, char *str);
 
 #endif
