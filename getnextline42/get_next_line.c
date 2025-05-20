eb960bf4455662e3ee7a0871a482d48c4f79a3ca
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-05-20 13:28:29 +0200

    Update get_next_line.c

--- getnextline42/get_next_line.c
+++ getnextline42/get_next_line.c
@@ -15,7 +15,7 @@
 char	*ft_read_before_newline(int fd, char *leftover)
 {
 	char	*buff;
-	size_t	read_bytes;
+	ssize_t	read_bytes;
 
 	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
