e7e2a5f2f08f5f270936b1a939395dc6f9cc1460
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-05-20 13:29:04 +0200

    Update get_next_line.h

--- getnextline42/get_next_line.h
+++ getnextline42/get_next_line.h
@@ -13,10 +13,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <sys/types.h>
-# include <sys/uio.h>
 # include <unistd.h>
 # include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_read_before_newline(int fd, char *leftover);
