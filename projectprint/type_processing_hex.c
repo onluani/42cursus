7be2fea1954939e1cfd55e8f2a55c5c8c48bf66d
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-05-27 15:51:22 +0200

    Update type_processing_hex.c

--- projectprint/type_processing_hex.c
+++ projectprint/type_processing_hex.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_processing_hex.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antsitsk <antsitsk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 12:25:23 by antsitsk          #+#    #+#             */
+/*   Updated: 2025/05/27 15:41:09 by antsitsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	x_type(va_list arguments, int *length, char *str)
@@ -10,10 +22,19 @@ void	x_type(va_list arguments, int *length, char *str)
 
 void	p_type(va_list arguments, int *length, char *str)
 {
+	void			*ptr;
 	unsigned long	adr;
 
-	adr = va_arg(arguments, unsigned long long);
-	ft_putchar('0', length);
-	ft_putchar('x', length);
-	ft_puthex(adr, length, str);
+	ptr = va_arg(arguments, void *);
+	if (!ptr)
+	{
+		ft_putstr("(nil)", length);
+	}
+	else
+	{
+		adr = (unsigned long)ptr;
+		ft_putchar('0', length);
+		ft_putchar('x', length);
+		ft_puthex(adr, length, str);
+	}
 }
