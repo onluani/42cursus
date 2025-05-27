bce15a2cb5356d7bbb55d5e256d3c2f107a6326b
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-05-27 15:50:12 +0200

    Update ft_printf.c

--- projectprint/ft_printf.c
+++ projectprint/ft_printf.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antsitsk <antsitsk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 11:46:24 by antsitsk          #+#    #+#             */
+/*   Updated: 2025/05/12 11:49:58 by antsitsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
 	int		length;
-	va_list	agruments;
+	va_list	arguments;
 
 	length = 0;
-	va_start(agruments, format);
-	length = parse_input(format, agruments, length);
-	va_end(agruments);
+	va_start(arguments, format);
+	length = parse_input(format, arguments, length);
+	va_end(arguments);
 	return (length);
 }
