6de3f525aaa1f29630adba4fb99d8e12a3d934bd
onlu☆ <153530190+onluani@users.noreply.github.com>
2025-05-27 15:50:27 +0200

    Update ft_printf.h

--- projectprint/ft_printf.h
+++ projectprint/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: antsitsk <antsitsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 16:24:01 by antsitsk          #+#    #+#             */
-/*   Updated: 2025/05/13 14:05:08 by antsitsk         ###   ########.fr       */
+/*   Updated: 2025/05/27 15:43:18 by antsitsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 int		ft_printf(const char *format, ...);
 void	parse_conversions(char *str, int *length, va_list arguments);
 int		parse_input(const char *str, va_list arguments, int length);
+void	ft_putstr(const char *s, int *length);
 int		ft_putchar(char c, int *length);
 void	ft_putnbr(int nb, int *length);
 void	ft_putuns(unsigned int nb, int *length);
